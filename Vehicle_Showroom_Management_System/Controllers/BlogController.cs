2ec1c7bdaf18ff3035d37116aa7d23f64ebc7f99
= <=>
2020-10-22 14:46:02 +0700

    Hoan thanh giao dien FE

---
+++ Vehicle_Showroom_Management_System/Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vehicle_Showroom_Management_System.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BlogDetails()
+        {
+            return View();
+        }
+    }
+}
