ca705e84c1d49790c1e651c53b0e39eb86223ae5
= <=>
2020-11-03 16:10:15 +0700

    fix date user and customer

--- Vehicle_Showroom_Management_System/Areas/Admin/Data/UserAccount.cs
+++ Vehicle_Showroom_Management_System/Areas/Admin/Data/UserAccount.cs
@@ -11,7 +11,8 @@ namespace Vehicle_Showroom_Management_System.Areas.Admin.Data
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class UserAccount
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,7 +20,7 @@ namespace Vehicle_Showroom_Management_System.Areas.Admin.Data
         {
             this.Customers = new HashSet<Customer>();
         }
-    
+
         public int UserId { get; set; }
         public string FullName { get; set; }
         public string UserName { get; set; }
@@ -27,11 +28,12 @@ namespace Vehicle_Showroom_Management_System.Areas.Admin.Data
         public string Address { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public System.DateTime Birthday { get; set; }
         public Nullable<System.DateTime> CreatedDate { get; set; }
         public Nullable<System.DateTime> UpdatedDate { get; set; }
         public Nullable<int> Status { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Customer> Customers { get; set; }
     }
