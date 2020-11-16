8f689fd04a322f670bcf378a2fff36ba631ba93b
= <=>
2020-11-17 00:27:13 +0700

    frond end

--- Vehicle_Showroom_Management_System/Controllers/HomeController.cs
+++ Vehicle_Showroom_Management_System/Controllers/HomeController.cs
@@ -3,14 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vehicle_Showroom_Management_System.Areas.Admin.Data;
 
 namespace Vehicle_Showroom_Management_System.Controllers
 {
     public class HomeController : Controller
     {
+        Vehicle_Showroom_Management_SystemEntities db = new Vehicle_Showroom_Management_SystemEntities();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult LoadData()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Car> listCars = db.Cars.ToList();
+            List<Image> listImages = db.Images.ToList();
+            var list = (from c in listCars
+                        join i in listImages on c.CarId equals i.CarId
+                        where i.Status == 1 && c.Checking == 0 && c.Sold == 0
+                        orderby c.CreatedDate descending
+                        select new
+                        {
+                            ModelNumberCar = c.ModelNumberCar,
+                            CarId = c.CarId,
+                            KilometerGone = c.KilometerGone,
+                            Engine = c.Engine,
+                            Gearbox = c.Gearbox,
+                            CarName = c.CarName,
+                            Color = c.Color,
+                            FuelConsumption = c.FuelConsumption,
+                            PurchaseOrderDate = c.PurchaseOrderDate,
+                            Status = c.Status,
+                            PriceOutput = c.PriceOutput,
+                            ImageName = i.Name
+                        }).Take(6).ToList();
+            var totalRow = list.Count;
+            return Json(new
+            {
+                data = list,
+                total = totalRow,
+                status = true
+            }, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult About()
